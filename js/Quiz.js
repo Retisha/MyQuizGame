ee838ac0e1a81f684801a6bd5768d7b61e270194
Retisha <76431390+Retisha@users.noreply.github.com>
2021-04-18 12:25:53 +0100

    Add files via upload
    add files

---
+++ js/Quiz.js
@@ -0,0 +1,72 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+    //write code here to hide question elements
+    question.hide();
+    //write code to change the background color here
+    background("white")
+    fill("Pink")
+    textSize(25)
+    text("Results of the quiz",340,50)
+    Contestant.getPlayerInfo();
+    if(allContestants !== undefined){
+      var pos = 230;
+     
+    }
+
+    fill("yellow")
+    textSize(20)
+    text("NOTE: Contestants who answered correct will be in green")
+
+    for(var plr in allContestants){
+      var correctAns = '2';
+      if(correctAns === allContestants[plr].answer)
+      fill("green")
+      else
+      fill("red");
+      text(allContestants[plr].name +':'+ allContestants[plr].answer ,150,pos)
+      pos = pos+20;
+    }
+  //}
+    //write code to show a heading for showing the result of Quiz
+
+    //call getContestantInfo( ) here
+
+
+    //write condition to check if contestantInfor is not undefined
+
+    //write code to add a note here
+
+    //write code to highlight contest who answered correctly
+    
+  }
+
+}
